a4bae80396138dcf35777726e5d95ce3e9375d8e
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-10-23 10:23:56 +0200

    Ejercicio Terminado

--- Boletin4_3/src/boletin4_3/Circulo.java
+++ Boletin4_3/src/boletin4_3/Circulo.java
@@ -22,4 +22,19 @@ public class Circulo {
         this.radio = radio;
         
     }
+    public double getRadio(){
+        return radio;
+    }
+    public void setRadio(double radio){
+        this.radio = radio;
+    }
+    
+    public double calcularArea(){
+        double area = PI * Math.pow(radio, 2);
+        return area;
+    }
+    public double calcularPerimetro(){
+        double perimetro = 2 * PI * radio;
+        return perimetro;
+    }
 }
