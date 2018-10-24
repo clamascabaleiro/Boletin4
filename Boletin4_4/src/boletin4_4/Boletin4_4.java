9e67445096676296805641b3be039674cf5981fc
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-10-24 10:24:52 +0200

    Ejercicio Terminado

---
+++ Boletin4_4/src/boletin4_4/Boletin4_4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_4;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Boletin4_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Comida comida1 = new Comida();
+        comida1.engadirPolbo(10);
+        comida1.engadirPatacas(10);
+        System.out.println(comida1.getPolbo());
+        System.out.println(comida1.getPatacas());
+        System.out.println("El número de clientes a los que se le puede " + "servir comida es de " + comida1.calcularClientes());
+    }
+   }
+    
---
+++ Boletin4_4/src/boletin4_4/Comida.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_4;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Comida {
+    private double polbo = 0;
+    private double patacas = 0;
+    
+    public double getPolbo(){
+        return polbo;
+    }
+    public double getPatacas(){
+        return patacas;
+    }
+    public void setPolbo(double polbo){
+        this.polbo = polbo;
+    }
+    public void setPatacas(double patacas){
+        this.patacas = patacas;
+    }
+    
+    public void engadirPolbo(int x){
+        polbo += x;
+    }
+    public void engadirPatacas(int x){
+        patacas += x;
+    }
+    public double calcularClientes(){
+        double contador = 0;
+        double polb = polbo;
+        double patac = patacas;
+        while(polb >= 2 && patac >= 1){
+            contador += 3;
+            polb -= 2;
+            patac -= 1;
+        }
+        return contador;    
+    }
+}
+
